ed149c32e0ed7f481d07d67b319fdeb2e9e042be
hhdang <hhdang@qq.com>
2016-08-24 11:15:48 +0800

    start

---
+++ Test_AutoKey/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Test_AutoKey
+{
+    partial class AutoKey
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.RichTextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 12);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(303, 75);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.Text = "run: D:\\Program Files\\ControlEase\\NX\\InRun.exe\nsleep: 3000\nkey: {ENTER}";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(46, 116);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 12);
+            this.lblInfo.TabIndex = 1;
+            // 
+            // AutoKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 153);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtCode);
+            this.Name = "AutoKey";
+            this.Text = "AutoKey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtCode;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
